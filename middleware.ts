1bb7bfd1931dd54bfb0cf4aeaedafa27b67389dc
Abhishek Agnihotri <abhishekagni2806@gmail.com>
2024-11-09 23:50:01 +0530

    solve the server issue

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,14 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-  publicRoutes : ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'], // Corrected to 'publicRoutes' instead of 'publicRoutes '
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)", // Matches all non-static routes
+    "/", // Matches the root
+    "/api/:path*", // Matches all API routes
+    "/trpc/:path*" // Matches all trpc routes if you're using tRPC
+  ],
 };
